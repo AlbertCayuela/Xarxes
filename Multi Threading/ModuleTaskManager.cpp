aee587c6344ac5e8b768d218aa4cd7d5310d951a
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-07 19:42:42 +0200

    THREADS DONE

--- Multi Threading/ModuleTaskManager.cpp
+++ Multi Threading/ModuleTaskManager.cpp
@@ -1,15 +1,35 @@
 #include "ModuleTaskManager.h"
 
+using namespace std;
 
 void ModuleTaskManager::threadMain()
 {
 	while (true)
 	{
 		// TODO 3:
-		// - Wait for new tasks to arrive
-		// - Retrieve a task from scheduledTasks
-		// - Execute it
-		// - Insert it into finishedTasks
+	// - Wait for new tasks to arrive
+	// - Retrieve a task from scheduledTasks
+	// - Execute it
+	// - Insert it into finishedTasks
+
+		Task* task = nullptr;
+
+			unique_lock<mutex> lock(mtx); //Lock the mutex
+			while (scheduledTasks.empty())
+			{
+				event.wait(lock);
+
+				if (exitFlag == true) 
+				{
+					return;
+				}
+			}
+
+			task = scheduledTasks.front();
+			scheduledTasks.pop();
+			task->execute();
+			finishedTasks.push(task);
+
 	}
 }
 
@@ -18,7 +38,7 @@ bool ModuleTaskManager::init()
 	// TODO 1: Create threads (they have to execute threadMain())
 	for (int i = 0; i < MAX_THREADS; ++i)
 	{
-		threads[i] = std::thread(&ModuleTaskManager::threadMain, this);
+		threads[i] = thread(&ModuleTaskManager::threadMain, this);
 	}
 
 	return true;
@@ -27,6 +47,12 @@ bool ModuleTaskManager::init()
 bool ModuleTaskManager::update()
 {
 	// TODO 4: Dispatch all finished tasks to their owner module (use Module::onTaskFinished() callback)
+	while (!finishedTasks.empty())
+	{
+		Task* task = finishedTasks.front();
+		task->owner->onTaskFinished(task);
+		finishedTasks.pop();
+	}
 
 	return true;
 }
@@ -34,6 +60,13 @@ bool ModuleTaskManager::update()
 bool ModuleTaskManager::cleanUp()
 {
 	// TODO 5: Notify all threads to finish and join them
+	exitFlag = true;
+	event.notify_all();
+	for (int i = 0; i < MAX_THREADS; ++i)
+	{
+		threads[i].join();
+	}
+
 
 	return true;
 }
@@ -43,4 +76,7 @@ void ModuleTaskManager::scheduleTask(Task *task, Module *owner)
 	task->owner = owner;
 
 	// TODO 2: Insert the task into scheduledTasks so it is executed by some thread
+	unique_lock<mutex> lock(mtx); //Lock the mutex
+	scheduledTasks.push(task);
+	event.notify_one();
 }
