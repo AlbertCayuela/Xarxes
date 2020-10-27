6254fc970231810863239db2e15ec1956562cfbb
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-27 12:57:25 +0100

    projects created properly

--- Sockets/Sockets/main.cpp
+++ Sockets/Sockets/main.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() 
+int main()
 {
 	printf("HELLO, THIS IS THE UDP CLIENT!!\n");
 
---
+++ Sockets/UDPServer/main.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("HELLO, THIS IS THE UDP SERVER!!\n");
+
+	system("pause");
+	return 0;
+}
