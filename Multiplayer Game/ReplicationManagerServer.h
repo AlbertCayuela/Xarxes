5779f1468cca385d2811a6cec09a6c2299274e26
AlbertCayuela <albertcayuela16@gmail.com>
2020-12-19 12:31:27 +0100

    Replicationmanagerserver.h prepared

--- Multiplayer Game/ReplicationManagerServer.h
+++ Multiplayer Game/ReplicationManagerServer.h
@@ -1,3 +1,31 @@
-#pragma once
+#ifndef _REPLICATIONMANAGERSERVER_
+#define _REPLICATIONMANAGERSERVER_
 
-// TODO(you): World state replication lab session
+enum class ReplicationAction 
+{
+	None,
+	Create,
+	Update,
+	Destroy
+};
+
+struct ReplicationCommand
+{
+	ReplicationAction action = ReplicationAction::None;
+	uint32 networkId;
+};
+
+class ReplicationManagerServer
+{
+public:
+
+	void Create(uint32 networkId);
+	void Update(uint32 networkId);
+	void Destroy(uint32 networkId);
+	void Write(OutputMemoryStream &packet);
+
+private:
+	std::vector<ReplicationCommand> commands;
+};
+
+#endif
